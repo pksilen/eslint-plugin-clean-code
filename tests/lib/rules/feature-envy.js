ade8a5b66f044a5107aa0acc547aa4738ef70913
Petri Silen <petri.silen@nokia.com>
2020-01-16 15:32:47 +0200

    Added test cases

--- tests/lib/rules/feature-envy.js
+++ tests/lib/rules/feature-envy.js
@@ -28,6 +28,9 @@ var ruleTester = new RuleTester();
 ruleTester.run('feature-envy', rule, {
 
     valid: [
+        "Number.isFinite(1); Number.isFinite(2); Number.isFinite(3);",
+        "const str = ''; str.trim(); str.trim(); str.trim()",
+        "import _ from 'lodash'; const a = []; a.head(); a.head(); a.head()",
         "class TestObj { testFunction() {}; }; const testObj = new TestObj(); function test() { testObj.testFunction(); testObj.testFunction(); }"
     ],
 
@@ -38,6 +41,28 @@ ruleTester.run('feature-envy', rule, {
                 message: "Feature envy: object 'testObj' called multiple times",
                 type: "FunctionDeclaration"
             }]
+        },
+        {
+            code: "import testObj from './testObj'; function test() { testObj.testFunction(); testObj.testFunction(); testObj.testFunction(); }",
+            errors: [{
+                message: "Feature envy: object 'testObj' called multiple times",
+                type: "FunctionDeclaration"
+            }]
+        },
+        {
+            code: "import StringUtils from './StringUtils'; function test() { StringUtils.testFunction(); StringUtils.testFunction(); StringUtils.testFunction(); }",
+            errors: [{
+                message: "Feature envy: object 'StringUtils' called multiple times",
+                type: "FunctionDeclaration"
+            }]
+        },
+        {
+            code: "import Chart from './Chart'; const chart = new Chart(); function test() { chart.testFunction(); chart.testFunction(); chart.testFunction(); }",
+            errors: [{
+                message: "Feature envy: object 'chart' called multiple times",
+                type: "FunctionDeclaration"
+            }]
         }
+
     ]
 });
